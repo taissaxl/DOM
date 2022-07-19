6e313e56a57ac5b57578f5dc99a9d4d9bf2dd466
Taissa Leal <taissa.leal@estudante.ufjf.br>
2022-07-19 20:40:08 -0300

    adiciona arquivos form1

--- form01.js
+++ form01.js
@@ -4,8 +4,14 @@ function criaElemento(cor, largura, altura){
     document.body.appendChild(eNovo);
 }
 
-criaElemento(
-    document.exm01.cor.value,
-    document.exm01.largura.value,
-    document.exm01.altura.value,
-);
+document.exm01.addEventListener("submit", submitListener);
+
+function submitListener(event){
+    event.preventDefault();
+    
+    criaElemento(
+        document.exm01.cor.value,
+        document.exm01.largura.value,
+        document.exm01.altura.value,
+        );
+}
